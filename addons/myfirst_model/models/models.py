df3b79fb0039374981bce457bba8f29091974fc0
ebouabbadi <ebouabbadi@gmail.com>
2024-11-22 16:07:07 +0100

    creating module

--- addons/myfirst_model/models/models.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import models, fields, api
-
-class Customer(models.Model):
-    _name = 'customer'
-    name = fields.Char()
-    lastName = fields.Char()
-    age=fields.Integer()
--- addons/qms/models/policy_component.py
+++
@@ -1,9 +0,0 @@
-from odoo import fields, models
-
-
-class PolicyComponent(models.Model):
-
-    _name = "qms.policy_component"
-    _description = "Policy Component"
-
-    name = fields.Char(required=True)
